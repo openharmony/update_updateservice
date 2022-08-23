f8731eda895931319193ec31098d4eda698f71d3
jackd320 <jack.dai@huawei.com>
2022-08-23 17:39:08 +0800

    Description: updater_sa on demand start, not start at boot Signed-off-by: 戴琨 <jack.dai@huawei.com>

--- interfaces/inner_api/engine/update_service_ondemand.cpp
+++ interfaces/inner_api/engine/update_service_ondemand.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -40,7 +40,7 @@ void UpdateServiceOnDemand::OnLoadSystemAbilitySuccess(int32_t systemAbilityId, 
 {
     ENGINE_LOGI("OnLoadSystemAbilitySuccess systemAbilityId: %{public}d, IRemoteObject result: %{public}s",
         systemAbilityId, (remoteObject != nullptr) ? "succeed" : "failed");
-    loadUpdaterSaStatus_ = LoadUpdaterSaStatus::SUCCESS;
+    loadUpdaterSaStatus_ = (remoteObject != nullptr) ? LoadUpdaterSaStatus::SUCCESS : LoadUpdaterSaStatus::FAIL;
 }
 
 void UpdateServiceOnDemand::OnLoadSystemAbilityFail(int32_t systemAbilityId)
